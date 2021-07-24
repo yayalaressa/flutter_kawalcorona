42de4c925dc58df3227b69d18ac52802614af6ff
Yaya Laressa <yayalaressa90@gmail.com>
2021-07-24 17:29:16 +0700

    upload first

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: ColorPalette.palette1,
       ),
-      home: KawalCrona(title: 'Flutter Kawal Corona'),
+      home: KawalCoronaApp(title: 'Flutter Kawal Corona'),
     );
   }
 }
 
-class KawalCrona extends StatefulWidget {
-  const KawalCrona({Key? key, required this.title}) : super(key: key);
+class KawalCoronaApp extends StatefulWidget {
+  const KawalCoronaApp({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _KawalCronaState createState() => _KawalCronaState();
+  KawalCoronaState createState() => KawalCoronaState();
 }
 
-class _KawalCronaState extends State<KawalCrona> {
+class KawalCoronaState extends State<KawalCoronaApp> {
   dynamic listDataKawalCorona;
 
   @override
@@ -38,67 +38,88 @@ class _KawalCronaState extends State<KawalCrona> {
     listDataKawalCorona = DataKawalCorona.getDataKawalCorona();
   }
 
+  Future<void> refreshDataKawalCorona() async {
+    setState(() {
+      listDataKawalCorona = DataKawalCorona.getDataKawalCorona();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder<dynamic>(
-          future: listDataKawalCorona,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                  // ignore: unnecessary_null_comparison
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, i) {
-                    final positif =
-                        "${snapshot.data[i]['attributes']['Kasus_Posi']}";
-                    final sembuh =
-                        "${snapshot.data[i]['attributes']['Kasus_Semb']}";
-                    return Card(
-                      child: ListTile(
-                          tileColor: ColorPalette.palette3,
-                          title: Text(
-                            "${snapshot.data[i]['attributes']['Provinsi']}",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: RichText(
-                            text: TextSpan(
-                              style: DefaultTextStyle.of(context).style,
-                              children: <TextSpan>[
-                                TextSpan(text: "Positif: "),
-                                TextSpan(
-                                  // ignore: unnecessary_brace_in_string_interps
-                                  text: "${positif}",
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: ' | ',
-                                ),
-                                TextSpan(
-                                  text: 'Sembuh: ',
-                                ),
-                                TextSpan(
-                                  // ignore: unnecessary_brace_in_string_interps
-                                  text: '${sembuh}',
-                                  style: TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )),
-                    );
-                  });
-            }
-            return Center(child: CircularProgressIndicator());
-          }),
+      body: _buildContent(),
     );
+  }
+
+  Widget _buildContent() {
+    return RefreshIndicator(
+      onRefresh: () => refreshDataKawalCorona(),
+      child: _loadDataKawalCorona(),
+    );
+  }
+
+  Widget _loadDataKawalCorona() {
+    return FutureBuilder<dynamic>(
+        future: listDataKawalCorona,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                // ignore: unnecessary_null_comparison
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, i) {
+                  final positif =
+                      "${snapshot.data[i]['attributes']['Kasus_Posi']}";
+                  final sembuh =
+                      "${snapshot.data[i]['attributes']['Kasus_Semb']}";
+                  return Card(
+                    child: ListTile(
+                        tileColor: ColorPalette.palette3,
+                        title: Text(
+                          "${snapshot.data[i]['attributes']['Provinsi']}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: <TextSpan>[
+                              TextSpan(text: "Positif: "),
+                              TextSpan(
+                                // ignore: unnecessary_brace_in_string_interps
+                                text: "${positif}",
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' | ',
+                              ),
+                              TextSpan(
+                                text: 'Sembuh: ',
+                              ),
+                              TextSpan(
+                                // ignore: unnecessary_brace_in_string_interps
+                                text: '${sembuh}',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
